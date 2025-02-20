b6234507a1e8dd6052a46d1e14e592206558aea2
Mehdi Marzban <marzban7272@gmail.com>
2025-02-20 15:35:16 +0330

    feat: Add authentication components with tabs and form handling

---
+++ src/components/features/auth/TabsAuth.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+
+interface TabsAuthProps {
+    SigninComponent: React.FC;
+    SignupComponent: React.FC;
+}
+const TabsAuth: React.FC<TabsAuthProps> = ({ SigninComponent, SignupComponent }) => {
+    return (
+        <Tabs>
+            <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="signin">ورود</TabsTrigger>
+                <TabsTrigger value="password">ثبت نام</TabsTrigger>
+            </TabsList>
+            <TabsContent value="signin">
+                <SigninComponent />
+            </TabsContent>
+            <TabsContent value="signup">
+                <SignupComponent />
+            </TabsContent>
+        </Tabs>
+    );
+};
+
+export default TabsAuth;
