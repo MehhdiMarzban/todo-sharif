64d585c99b1746d44d7bc78e9fffcdd49da9cb27
Mehdi Marzban <marzban7272@gmail.com>
2025-02-20 19:21:20 +0330

    feat: Implement Signup component with form handling and validation, and enhance TabsAuth layout

--- src/components/features/auth/Signup.tsx
+++ src/components/features/auth/Signup.tsx
@@ -1,5 +1,102 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AuthType, authValidation } from "@/validations/auth";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 const Signup: React.FC = () => {
-    return <div className="">signup</div>;
+    const form = useForm<AuthType>({
+        resolver: zodResolver(authValidation),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = (values: AuthType) => {
+        console.log(values);
+    };
+
+    return (
+        <Card className="rounded-t-none">
+            <CardHeader>
+                <CardTitle>ثبت نام کاربر جدید</CardTitle>
+                <CardDescription>
+                    خوش آمدید، لطفا اطلاعات خود را وارد کنید
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem className="max-w-sm w-full mx-auto">
+                                    <FormLabel>نام کاربری</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            dir="ltr"
+                                            className="text-center"
+                                            placeholder="نام کاربری"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem className="max-w-sm w-full mx-auto">
+                                    <FormLabel>رمز عبور</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            dir="ltr"
+                                            className="text-center"
+                                            placeholder="رمزعبور"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+            </CardContent>
+            <CardFooter>
+                <Button
+                    variant={"default"}
+                    type="submit"
+                    className="max-w-sm w-full mx-auto"
+                    onClick={form.handleSubmit(onSubmit)}>
+                    ثبت نام
+                </Button>
+            </CardFooter>
+        </Card>
+    );
 };
 
 export default Signup;
--- src/validations/auth.ts
+++ src/validations/auth.ts
@@ -1,8 +1,19 @@
-import { z } from 'zod';
+import { z } from "zod";
+
+//* regex for english text with number and common symbols
+const englishPattern = /^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]+$/;
 
 export const authValidation = z.object({
-    username: z.string().min(3, "نام کاربری باید حداقل 3 کاراکتر باشد").max(20, "نام کاربری باید حداکثر 20 کاراکتر باشد"),
-    password: z.string().min(6, "رمز عبور باید حداقل 6 کاراکتر باشد").max(20, "رمز عبور باید حداکثر 20 کاراکتر باشد"),
+    username: z
+        .string()
+        .min(3, "نام کاربری باید حداقل 3 کاراکتر باشد")
+        .max(20, "نام کاربری باید حداکثر 20 کاراکتر باشد")
+        .regex(englishPattern, "متن فقط میتواند شامل حروف انگلیسی، اعداد و نمادهای معمولی باشد"),
+    password: z
+        .string()
+        .min(6, "رمز عبور باید حداقل 6 کاراکتر باشد")
+        .max(20, "رمز عبور باید حداکثر 20 کاراکتر باشد")
+        .regex(englishPattern, "متن فقط میتواند شامل حروف انگلیسی، اعداد و نمادهای معمولی باشد"),
 });
 
-type AuthType = z.infer<typeof authValidation>;
+export type AuthType = z.infer<typeof authValidation>;
