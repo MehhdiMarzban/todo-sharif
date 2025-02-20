c02763cb8215e6e47be840579218355550b5d0cb
Mehdi Marzban <marzban7272@gmail.com>
2025-02-20 20:09:17 +0330

    fix: Reorder imports in authentication page for consistency

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,11 @@
-import { Auth } from "@/components/features";
 import { Metadata } from "next";
+
+import { Auth } from "@/components/features";
+
 export const metadata : Metadata = {
     title: "احراز هویت",
 }
+
 export default () => {
     return <Auth />;
 };
