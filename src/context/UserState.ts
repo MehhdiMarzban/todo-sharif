675abf880947d4c78f7f84412cc45e03c868713f
Mehdi Marzban <marzban7272@gmail.com>
2025-02-20 21:00:01 +0330

    feat: Implement user state management with zustand and define user types

---
+++ src/context/UserState.ts
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+import { User } from "@/types";
+
+//* type definitions
+export type UserState = {
+    users: User[];
+    currentUser?: User;
+    totalUsers: number;
+};
+
+export type UserActions = {
+    signupUser: (user: User) => void;
+    signinUser: (user: User) => void;
+    removeUser: (id: string) => void;
+};
+
+export type UserStore = UserState & UserActions;
+
+//* initial zustand
+const useUserStore = create<UserStore>()(
+    persist(
+        (set) => ({
+            users: [],
+            currentUser: undefined,
+            totalUsers: 0,
+            signupUser: (user: User) =>
+                set((state) => {
+                    const isExist = state.users.find((u) => u.username === user.username);
+                    if (isExist) {
+                        return {};
+                    } else {
+                        return {
+                            users: [...state.users, user],
+                            totalUsers: state.users.length,
+                        };
+                    }
+                }),
+            signinUser: (user: User) =>
+                set((state) => {
+                    const currentUser = state.users.find(
+                        (u) => u.username === user.username && u.password === user.password
+                    );
+                    if (currentUser) {
+                        return {
+                            currentUser,
+                        };
+                    } else {
+                        return {
+                            currentUser: undefined,
+                        };
+                    }
+                }),
+            removeUser: (id: string) =>
+                set((state) => {
+                    return {
+                        users: state.users.filter((user) => user.id !== id),
+                        totalUsers: state.users.length,
+                    };
+                }),
+        }),
+        {
+            name: "sharif-user-storage", //* storage name
+            storage: createJSONStorage(() => localStorage), //* save and read from local storage
+        }
+    )
+);
+
+export default useUserStore;
