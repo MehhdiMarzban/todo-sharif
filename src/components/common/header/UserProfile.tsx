3e68e2fb5ed53d44a3e423a61f5fc37adcdc10a1
Mehdi Marzban <marzban7272@gmail.com>
2025-02-21 01:19:12 +0330

    feat: refactor UserProfile component to utilize useIsUserLoaded hook for improved user state management and loading indication

--- src/components/common/header/UserProfile.tsx
+++ src/components/common/header/UserProfile.tsx
@@ -1,22 +1,37 @@
 "use client";
 import Image from "next/image";
 import { useTransition } from "react";
-import { LogOut, MailIcon } from "lucide-react";
+import { LogOut } from "lucide-react";
 
 import { LoadingIcon } from "@/components/common";
 import { Button } from "@/components/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import useUserStore from "@/stores/UserState";
+import useIsUserLoaded from "@/hooks/useIsUserLoaded";
+import Link from "next/link";
 
-interface UserProfileProps {
-    username: string;
-    logout: () => void;
-}
-const UserProfile: React.FC<UserProfileProps> = ({ username, logout }) => {
+const UserProfile: React.FC = () => {
     const [pending, mutate] = useTransition();
+
+    const { currentUser, logoutUser } = useUserStore();
+    const { loading } = useIsUserLoaded();
+
+    if (loading) {
+        return <LoadingIcon className="size-9" />;
+    }
+
+    if (!currentUser) {
+        return (
+            <Button asChild variant={"ghost"}>
+                <Link href={"/auth"}>ورود / ثبت نام</Link>
+            </Button>
+        );
+    }
+
     const handleLogout = () => {
         mutate(() => {
-            logout();
+            logoutUser();
         });
     };
     return (
@@ -27,17 +42,17 @@ const UserProfile: React.FC<UserProfileProps> = ({ username, logout }) => {
                     height={25}
                     sizes="25px"
                     quality={100}
-                    alt={username}
+                    alt={currentUser.username}
                     src="/images/profile.png"
                     className="size-9 flex-shrink-0 rounded-full border bg-background shadow-sm cursor-pointer"
                 />
             </PopoverTrigger>
             <PopoverContent className="flex w-48 flex-col gap-2 p-2" align={"end"}>
-                <p className="font-semibold text-sm text-center">کاربر : {username}</p>
+                <p className="font-semibold text-sm text-center">کاربر : {currentUser.username}</p>
                 <Separator className="bg-border/40" />
                 <Button
                     size={"sm"}
-                    className="w-full focus-visible:ring-0" 
+                    className="w-full focus-visible:ring-0"
                     disabled={pending}
                     onClick={handleLogout}
                     variant={"destructive"}>
--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -5,23 +5,16 @@ import { Logo } from "./Logo";
 import { Button } from "@/components/ui/button";
 import useUserStore from "@/stores/UserState";
 import UserProfile from "./UserProfile";
+import useIsUserLoaded from "@/hooks/useIsUserLoaded";
 
 const Header: React.FC = () => {
-    const { currentUser, logoutUser } = useUserStore();
     return (
         <header className="sticky top-0 z-50 border-b bg-card">
             <nav className="container flex items-center justify-between gap-4 px-4 py-2">
                 <Logo />
                 <div className="flex items-center gap-4">
-                    {/* <TodoModal />
-            <UserProfile  /> */}
-                    {currentUser ? (
-                        <UserProfile username={currentUser.username} logout={logoutUser} />
-                    ) : (
-                        <Button asChild variant={"ghost"}>
-                            <Link href={"/auth"}>ورود / ثبت نام</Link>
-                        </Button>
-                    )}
+                    {/* <TodoModal /> */}
+                    <UserProfile />
                 </div>
             </nav>
         </header>
