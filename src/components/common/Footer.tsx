fbd976d8819698bdd06a11029dd17867df838ee8
Mehdi Marzban <marzban7272@gmail.com>
2025-02-20 14:31:41 +0330

    fix: Enhance Footer component to display current year alongside author text

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,14 @@
 import { siteConfig } from "@/config/site";
 
-const Footer : React.FC = () => {
+const Footer: React.FC = () => {
     return (
         <footer className="bg-primary text-primary-foreground text-center py-1">
-            <p>{siteConfig.authorText}</p>
+            <div className="flex flex-row items-center justify-center gap-1">
+                <span>{siteConfig.authorText}</span>
+                <span className="font-bold">{new Date().getFullYear()}</span>
+            </div>
         </footer>
     );
-}
+};
 
 export default Footer;
