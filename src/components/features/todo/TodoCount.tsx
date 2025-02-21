7b32f102261a2108d2d14df2343996ce50919a5f
Mehdi Marzban <marzban7272@gmail.com>
2025-02-22 00:34:56 +0330

    feat: update TodoCount component to display the number of todos for the current user

--- src/components/features/todo/TodoCount.tsx
+++ src/components/features/todo/TodoCount.tsx
@@ -3,11 +3,11 @@ import useAppStore from "@/stores/AppState";
 import NumberFlow from "@number-flow/react";
 
 const TodoCount: React.FC = () => {
-    const { todos } = useAppStore();
+    const { currentUser } = useAppStore();
     return (
         <h2 className="text-right font-medium text-xs sm:text-sm bg-secondary text-secondary-foreground p-2 rounded-sm">
             {" تسک ها : "}
-            <NumberFlow aria-hidden willChange format={{ useGrouping: false }} value={todos.length} />
+            <NumberFlow aria-hidden willChange format={{ useGrouping: false }} value={currentUser?.todos?.length || 0} />
         </h2>
     );
 };
