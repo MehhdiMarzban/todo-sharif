5d1f0ec4fc653af33a19704a4cafde36cb60cb02
Mehdi Marzban <marzban7272@gmail.com>
2025-02-23 10:44:27 +0330

    fix: adjust text sizes and spacing in header components for better responsiveness

--- src/components/common/header/Logo.tsx
+++ src/components/common/header/Logo.tsx
@@ -9,7 +9,7 @@ import { siteConfig } from "@/config/site";
  * When clicked, it navigates to the home page.
  */
 export const Logo = () => (
-    <Link href={"/"} className="flex items-center gap-1 text-lg font-bold text-primary">
+    <Link href={"/"} className="flex items-center gap-1 text-sm sm:text-base md:text-lg font-bold text-primary">
         <BadgeCheck className="size-5 align-middle" strokeWidth={2} />
         {siteConfig.title}
     </Link>
