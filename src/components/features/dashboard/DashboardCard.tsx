f7b7bb82ca82c636475aed43b737006e4f25a54d
Mehdi Marzban <marzban7272@gmail.com>
2025-03-06 18:53:42 +0330

    feat: fix DashboardCard number style

--- src/components/features/dashboard/DashboardCard.tsx
+++ src/components/features/dashboard/DashboardCard.tsx
@@ -14,14 +14,6 @@ interface DashboardCardProps {
  * @property {number} count - The count to be displayed on the card.
  * @property {React.FC<React.ComponentProps<"svg">>} Icon - The icon component to be displayed on the card.
  */
-
-/**
- * A card component for displaying dashboard information.
- *
- * @component
- * @param {DashboardCardProps} props - The props for the DashboardCard component.
- * @returns {JSX.Element} The rendered DashboardCard component.
- */
 const DashboardCard: React.FC<DashboardCardProps> = ({ title, count, Icon }) => {
     return (
         <Card className="relative overflow-hidden bg-background/80 transition-all ease backdrop-blur-md border border-gray-200/30 dark:border-gray-800/50 group animate-in zoom-in fade-in">
@@ -32,10 +24,10 @@ const DashboardCard: React.FC<DashboardCardProps> = ({ title, count, Icon }) => 
                     </div>
                     <h3 className="text-base md:text-lg font-semibold">{title}</h3>
                 </div>
-                <p className="text-4xl font-bold bg-gradient-to-r from-primary to-blue-600 bg-clip-text text-transparent">
+                <p className="text-4xl font-bold bg-gradient-to-b from-primary to-blue-600 bg-clip-text text-transparent">
                     {count}
                 </p>
-                <div className="absolute -bottom-8 -right-8 h-24 w-24 rounded-full bg-primary/10 blur-xl" />
+                <div className="absolute -bottom-8 -right-8 h-24 w-24 rounded-full bg-primary/20 blur-xl" />
             </div>
         </Card>
     );
